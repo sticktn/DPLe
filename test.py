ec29f9c7b813e4e89081fad492dba3ed4d078643
fwq <1822098581@qq.com>
2023-10-25 11:04:23 +0800

    python ONNX model dep[loy

--- test.py
+++ test.py
@@ -3,39 +3,35 @@ import onnxruntime
 import numpy
 import cv2
 
+class ONNX_Engine:
+    def __init__(self,onnx_model_path):
+        self.onnx_model_path = onnx_model_path
+        self.input_names = []
+        self.output_names = []
+        self.input_shapes = []
+        self.output_shapes = []
+        self.session = onnxruntime.InferenceSession(self.onnx_model_path)
+        input_tensors = self.session.get_inputs()
+        for input_tensor in input_tensors:
+            self.input_names.append(input_tensor.name)
+            self.input_shapes.append(input_tensor.shape)
+        output_tensors = self.session.get_outputs()
+        for output_tensor in output_tensors:
+            self.output_names.append(output_tensor.name)
+            self.output_shapes.append(output_tensor.shape)
 
-class onnx_model:
-    def __init__(self, onnx_path):
-        self.model = onnxruntime.InferenceSession(onnx_path)
-        self.input_name = self.model.get_inputs()[0].name
+    def inference(self,input_tensor):
+        input_dict = {}
+        for _input_tensor,_input_name in zip(input_tensor,self.input_names):
+            input_dict[_input_name] = _input_tensor
+        outputs = self.session.run(self.output_names,input_dict)
+        for i in np.arange(len(outputs)):
+            outputs[i] = np.reshape(outputs[i], self.output_shapes[i])
+        return outputs
 
-    def get_model_img(self, img_path):
-        pass
-
-    def run(self, img):
-        pass
-
-
-class two_label_model(onnx_model):
-    def __init__(self, onnx_path):
-        super().__init__(onnx_path)
-        self.output_names = ['bird_name_index', 'color_index']
-        self.img_size = (3, 224, 224)
-
-    def get_model_img(self, img_path):
-        img = cv2.imread(img_path)
-        img = cv2.resize(img, self.img_size)
-        return img
-
-    def run(self, img):
-        if isinstance(img, str):
-            img = self.get_model_img(img)
-        else:
-            outputs = self.model.run(None, {self.input_name: img})
-            return {i: np.argmax(j) for (i, j) in zip(self.output_names, outputs)}
 
 
 if __name__ == "__main__":
-    model = two_label_model('./onnx_model/two_label_classify_batch1.onnx')
+    model = ONNX_Engine('./onnx_model/two_label_classify_batch1.onnx')
     input_ndarray = np.random.randn(1, 3, 224, 224).astype(dtype=np.float32)
-    print(model.run(input_ndarray))
+    print(model.inference([input_ndarray]))
